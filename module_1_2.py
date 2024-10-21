3cee5fd9ec0d0deb87b89690af89acf15dc9e92e
olgazharskaya58 <olgazharskaya58@gmail.com>
2024-10-21 17:36:09 +0500

    добавлено задание 1.2.

---
+++ module_1_2.py
@@ -0,0 +1,7 @@
+#02.01
+number_of_completed_tasks = 12
+number_of_hours_spent = 1.5
+course_name = 'Python'
+time_per_task = (number_of_hours_spent / number_of_completed_tasks)
+print('Курс:', course_name, ', затрачено часов:', number_of_hours_spent, ', всего задач:',
+      number_of_completed_tasks, ', среднее время выполнения:', time_per_task,'часа')
