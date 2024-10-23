1e0046a1468186ee6f8b6114c8b3658ab8aadd5e
olgazharskaya58 <olgazharskaya58@gmail.com>
2024-10-23 22:26:08 +0500

    добавлено задание 1.4.

---
+++ module_1_4.py
@@ -0,0 +1,15 @@
+
+my_string = input('Введите строку с произвольным текстом: ')
+
+print('Количество символов введённого текста: ', len(my_string))
+
+print(my_string .upper())
+
+print(my_string .lower())
+
+print(my_string .replace(' ', ''))
+
+print(my_string [0])
+
+print(my_string [-1])
+
