caa67b019f566e0fb1348be234d2cb389df199a9
olgazharskaya58 <olgazharskaya58@gmail.com>
2024-10-23 22:35:38 +0500

    Добавлено задание 1.5

---
+++ module_1_5.py
@@ -0,0 +1,10 @@
+
+immutable_var = (False, 4, 12, '100%', 5)
+print(immutable_var)
+
+mutable_list = ['Равенство', 15, True, 65, 'f']
+print(mutable_list)
+
+mutable_list.append('D')
+mutable_list[2] = False
+print(mutable_list)
